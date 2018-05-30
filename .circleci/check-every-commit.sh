6f1452a07b2bf8087fb54d725ea6c4e1ac1f3ee3
Stephane Angel (Twidi) <s.angel@twidi.com>
2018-05-30 11:24:03 +0200

    Use correct make target in `check-every-commit.sh`

--- .circleci/check-every-commit.sh
+++ .circleci/check-every-commit.sh
@@ -62,7 +62,7 @@ GIST_ACCOUNT="mixtbot"
 GIST_ID="d1d436a981fd4e50c64a6acc8a51d6d2"
 CI="circleci"
 TOOL_NAME="commit-tester"
-MAKE_TARGETS="check test"
+MAKE_TARGETS="lint test"
 POST_FINAL_STATUS="${POST_FINAL_STATUS:-FALSE}"
 
 # End of configuration
