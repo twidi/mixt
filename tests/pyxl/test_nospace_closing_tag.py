f76c430425f8fcd8e9b12a3b7cbdfa877e95a207
Stephane Angel (Twidi) <s.angel@twidi.com>
2018-05-30 18:42:16 +0200

    Add a test to ensure the space before "/" is optional

---
+++ tests/pyxl/test_nospace_closing_tag.py
@@ -0,0 +1,49 @@
+# coding: mixt
+
+"""Ensure that the space before the ``/`` character is not mandatory"""
+
+from mixt.pyxl import html
+from mixt.pyxl.element import Element
+
+
+def test_normal_tag_without_attributes():
+    assert str(<button />) == '<button></button>'
+    assert str(<button/>) == '<button></button>'
+
+
+def test_normal_tag_with_attributes():
+    assert str(<button name="foo" />) == '<button name="foo"></button>'
+    assert str(<button name="foo"/>) == '<button name="foo"></button>'
+
+
+def test_nochild_tag_without_attributes():
+    assert str(<link />) == '<link />'
+    assert str(<link/>) == '<link />'
+
+
+def test_nochild_tag_with_attributes():
+    assert str(<link rel="foo" />) == '<link rel="foo" />'
+    assert str(<link rel="foo"/>) == '<link rel="foo" />'
+
+
+def test_new_element_without_attributes():
+    class Foo(Element):
+
+        def render(self):
+            return <div data-name="foo"/>
+
+    assert str(<Foo />) == '<div data-name="foo"></div>'
+    assert str(<Foo/>) == '<div data-name="foo"></div>'
+
+
+def test_new_element_with_attributes():
+    class Foo(Element):
+        __attrs__ = {
+            'name': str
+        }
+
+        def render(self):
+            return <div data-name="{self.name}"/>
+
+    assert str(<Foo name="foo" />) == '<div data-name="foo"></div>'
+    assert str(<Foo name="foo"/>) == '<div data-name="foo"></div>'
