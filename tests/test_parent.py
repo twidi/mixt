3c5d19deb20b8f538de59bd84f8460bd6fa33610
Stephane Angel (Twidi) <s.angel@twidi.com>
2018-06-12 12:18:19 +0200

    Add tests for append/prepend

--- tests/test_parent.py
+++ tests/test_parent.py
@@ -93,3 +93,201 @@ def test_parent_via_encapsulated_fragments_and_siblings():
     br, = span1.__children__
     assert br.__parent__ is span1
     assert len(span2.__children__) == 0
+
+
+def test_append_one_el_to_empty():
+    parent = <div />
+    parent_children = parent.__children__
+    child = <span />
+    assert parent.__children__ == []
+    parent.append(child)
+    assert parent.__children__ == [child]
+    assert parent_children is parent.__children__
+    assert child.__parent__ is parent
+    assert str(parent) == '<div><span></span></div>'
+
+
+def test_append_one_string_to_empty():
+    parent = <div />
+    parent_children = parent.__children__
+    child = "foo"
+    parent.append(child)
+    assert parent.__children__ == [child]
+    assert parent_children is parent.__children__
+    assert str(parent) == '<div>foo</div>'
+
+
+def test_prepend_one_el_to_empty():
+    parent = <div />
+    parent_children = parent.__children__
+    child = <span />
+    parent.prepend(child)
+    assert parent.__children__ == [child]
+    assert parent_children is parent.__children__
+    assert child.__parent__ is parent
+    assert str(parent) == '<div><span></span></div>'
+
+
+def test_prepend_one_string_to_empty():
+    parent = <div />
+    parent_children = parent.__children__
+    child = "foo"
+    parent.prepend(child)
+    assert parent.__children__ == [child]
+    assert parent_children is parent.__children__
+    assert str(parent) == '<div>foo</div>'
+
+
+def test_append_many_to_empty():
+    parent = <div />
+    parent_children = parent.__children__
+    child1 = <span />
+    child2 = "foo"
+    parent.append([child1, child2])
+    assert parent.__children__ == [child1, child2]
+    assert parent_children is parent.__children__
+    assert child1.__parent__ is parent
+    assert str(parent) == '<div><span></span>foo</div>'
+
+
+def test_prepend_many_to_empty():
+    parent = <div />
+    parent_children = parent.__children__
+    child1 = <span />
+    child2 = "foo"
+    parent.prepend([child1, child2])
+    assert parent.__children__ == [child1, child2]
+    assert parent_children is parent.__children__
+    assert child1.__parent__ is parent
+    assert str(parent) == '<div><span></span>foo</div>'
+
+
+def test_append_many_levels_list_to_empty():
+    parent = <div />
+    parent_children = parent.__children__
+    child1 = <span />
+    child2 = "foo"
+    child3 = <br />
+    parent.append([child1, [child2, child3]])
+    assert parent.__children__ == [child1, child2, child3]
+    assert parent_children is parent.__children__
+    assert child1.__parent__ is parent
+    assert child3.__parent__ is parent
+    assert str(parent) == '<div><span></span>foo<br /></div>'
+
+
+def test_prepend_many_levels_list_to_empty():
+    parent = <div />
+    parent_children = parent.__children__
+    child1 = <span />
+    child2 = "foo"
+    child3 = <br />
+    parent.prepend([child1, [child2, child3]])
+    assert parent.__children__ == [child1, child2, child3]
+    assert parent_children is parent.__children__
+    assert child1.__parent__ is parent
+    assert child3.__parent__ is parent
+    assert str(parent) == '<div><span></span>foo<br /></div>'
+
+
+def test_append_one_el_to_non_empty():
+    first_child = <div />
+    parent = <div>{first_child}</div>
+    parent_children = parent.__children__
+    child = <span />
+    assert parent.__children__ == [first_child]
+    parent.append(child)
+    assert parent.__children__ == [first_child, child]
+    assert parent_children is parent.__children__
+    assert child.__parent__ is parent
+    assert str(parent) == '<div><div></div><span></span></div>'
+
+
+def test_append_one_string_to_non_empty():
+    first_child = <div />
+    parent = <div>{first_child}</div>
+    parent_children = parent.__children__
+    child = "foo"
+    parent.append(child)
+    assert parent.__children__ == [first_child, child]
+    assert parent_children is parent.__children__
+    assert str(parent) == '<div><div></div>foo</div>'
+
+
+def test_prepend_one_el_to_non_empty():
+    first_child = <div />
+    parent = <div>{first_child}</div>
+    parent_children = parent.__children__
+    child = <span />
+    parent.prepend(child)
+    assert parent.__children__ == [child, first_child]
+    assert parent_children is parent.__children__
+    assert child.__parent__ is parent
+    assert str(parent) == '<div><span></span><div></div></div>'
+
+
+def test_prepend_one_string_to_non_empty():
+    first_child = <div />
+    parent = <div>{first_child}</div>
+    parent_children = parent.__children__
+    child = "foo"
+    parent.prepend(child)
+    assert parent.__children__ == [child, first_child]
+    assert parent_children is parent.__children__
+    assert str(parent) == '<div>foo<div></div></div>'
+
+
+def test_append_many_to_non_empty():
+    first_child = <div />
+    parent = <div>{first_child}</div>
+    parent_children = parent.__children__
+    child1 = <span />
+    child2 = "foo"
+    parent.append([child1, child2])
+    assert parent.__children__ == [first_child, child1, child2]
+    assert parent_children is parent.__children__
+    assert child1.__parent__ is parent
+    assert str(parent) == '<div><div></div><span></span>foo</div>'
+
+
+def test_prepend_many_to_non_empty():
+    first_child = <div />
+    parent = <div>{first_child}</div>
+    parent_children = parent.__children__
+    child1 = <span />
+    child2 = "foo"
+    parent.prepend([child1, child2])
+    assert parent.__children__ == [child1, child2, first_child]
+    assert parent_children is parent.__children__
+    assert child1.__parent__ is parent
+    assert str(parent) == '<div><span></span>foo<div></div></div>'
+
+
+def test_append_many_levels_list_to_non_empty():
+    first_child = <div />
+    parent = <div>{first_child}</div>
+    parent_children = parent.__children__
+    child1 = <span />
+    child2 = "foo"
+    child3 = <br />
+    parent.append([child1, [child2, child3]])
+    assert parent.__children__ == [first_child, child1, child2, child3]
+    assert parent_children is parent.__children__
+    assert child1.__parent__ is parent
+    assert child3.__parent__ is parent
+    assert str(parent) == '<div><div></div><span></span>foo<br /></div>'
+
+
+def test_prepend_many_levels_list_to_non_empty():
+    first_child = <div />
+    parent = <div>{first_child}</div>
+    parent_children = parent.__children__
+    child1 = <span />
+    child2 = "foo"
+    child3 = <br />
+    parent.prepend([child1, [child2, child3]])
+    assert parent.__children__ == [child1, child2, child3, first_child]
+    assert parent_children is parent.__children__
+    assert child1.__parent__ is parent
+    assert child3.__parent__ is parent
+    assert str(parent) == '<div><span></span>foo<br /><div></div></div>'
